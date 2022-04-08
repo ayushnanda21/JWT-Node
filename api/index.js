88daa0e33a381ba9d47cbee1fb839449d0cc2617
Ayush Nanda <ananda_be18@thapar.edu>
2022-04-09 00:08:42 +0530

    jwt:feat

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config()
 const express = require("express")
 const bodyParser = require("body-parser")
+const jwt = require("jsonwebtoken")
 
 const app = express();
 app.use(express.json())
@@ -29,13 +30,50 @@ app.post("/api/login", (req,res)=>{
     });
 
     if(user){
-        res.status(200).json(user);
+        //Generate access token
+        const accessToken = jwt.sign(
+            {id: user.id , isAdmin : user.isAdmin},
+            process.env.JWT_SECRET_KEY
+            )
+        res.status(200).json({
+            username: user.username ,
+            isAdmin : user.isAdmin,
+            accessToken
+        });
     }
     else{
         res.status(400).json('Username or password incorrect')
     }
 })
+
+//verifying jwt token
+const verify  =(req,res,next)=>{
+    const authHeader  =req.headers.authorization;
+    if(authHeader){
+        const token  =  authHeader.split(" ")[1];
+
+        jwt.verify(token , process.env.JWT_SECRET_KEY, (err, user)=>{
+            if(err){
+                return res.status(403).json('Token is not valid')
+            } 
+
+            req.user = user;
+            next();
+        })
+    } else{
+        res.status(401).json("You are not authenticated")
+    }
+}
     
+
+//delete user route
+app.delete("/api/users/:userId" ,verify , (req,res)=>{
+    if(req.user.id === req.params.userId || req.user.isAdmin){
+        res.status(200).json("user has been deleted")
+    } else{
+        res.status(403).json("You are not allowed to delete this user")
+    }
+})
 
 //server
 app.listen(5000, function(req,res){
